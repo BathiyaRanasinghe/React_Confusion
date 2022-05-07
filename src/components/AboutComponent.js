1e2d70fb7c6b2975a2dfc2f51163738ca2869808
bathiyaranasinghe <bar407@usask.ca>
2022-05-07 00:46:19 -0600

    A4 Part3

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -3,22 +3,25 @@ import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'r
 import { Link } from 'react-router-dom';
 import { baseUrl } from '../shared/baseUrl';
 import { Loading } from './LoadingComponent';
+import { Fade, Stagger } from 'react-animation-components';
 
 function RenderLeader({leader}) {
-   return (
-      <div key={leader.id} className="col-12 mt-5">
-         <Media tag="li">
-            <Media left middle>
-                <Media object src={baseUrl + leader.image} alt={leader.name} />
-            </Media>
-            <Media body className="ml-5">
-               <Media heading>{leader.name}</Media>
-               <p>{leader.designation}</p>
-               <p>{leader.description}</p>
-            </Media>
-         </Media>
-      </div>
-   );
+    return (
+        <Fade in>
+            <div key={leader.id} className="col-12 mt-5">
+                <Media tag="li">
+                    <Media left middle>
+                        <Media object src={baseUrl + leader.image} alt={leader.name} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>{leader.name}</Media>
+                        <p>{leader.designation}</p>
+                        <p>{leader.description}</p>
+                    </Media>
+                </Media>
+            </div>
+        </Fade>
+    );
 }
 
 function About(props) {
@@ -105,7 +108,9 @@ function About(props) {
                     </div>
                     <div className="col-12">
                         <Media list>
-                            {leaders}
+                            <Stagger in>
+                                {leaders}
+                            </Stagger>
                         </Media>
                     </div>
                 </div>
